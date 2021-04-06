4e7395df9c507c917c643a8c6119845d368bd66f
Daniel Parsley <danielparsley92@gmail.com>
2021-04-06 08:52:08 -0400

    commit tree 24d25c3c90c92322ae4d18cc408f7f6807885466

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const hq = 42
+function distanceFromHqInBlocks(blocks){
+    if (blocks > hq){
+        return(blocks - hq)
+    } else if (blocks < hq){
+        return(hq - blocks)
+    }
+}
+
+function distanceFromHqInFeet(){
+
+}
+
+function distanceTravelledInFeet(){
+
+}
+
+function calculatesFarePrice(){
+
+}
